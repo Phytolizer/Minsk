53c4ab394b5666423302b2d02646d41c1f625d1b
Kyle Coffey <me@phytolizer.dev>
2022-03-16 13:47:57 -0500

    old-style class

--- mc/Program.cs
+++ mc/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace mc;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+    }
+}
