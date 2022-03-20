94e0f79d02256e14289b56a5288eb52c24f8e81f
Kyle Coffey <me@phytolizer.dev>
2022-03-20 11:43:33 -0500

    crude evaluator

---
+++ Minsk/Evaluator.cs
@@ -0,0 +1,49 @@
+namespace Minsk;
+
+public class Evaluator
+{
+    private readonly ExpressionSyntax _root;
+
+    public Evaluator(ExpressionSyntax root)
+    {
+        _root = root;
+    }
+
+    public int Evaluate()
+    {
+        return EvaluateExpression(_root);
+    }
+
+    private int EvaluateExpression(ExpressionSyntax root)
+    {
+        // ReSharper disable once SwitchExpressionHandlesSomeKnownEnumValuesWithExceptionInDefault
+        return root.Kind switch
+        {
+            SyntaxKind.LiteralExpression => EvaluateLiteralExpression((LiteralExpressionSyntax)root),
+            SyntaxKind.BinaryExpression => EvaluateBinaryExpression((BinaryExpressionSyntax)root),
+            _ => throw new InvalidOperationException($"Unexpected syntax node {root.Kind}")
+        };
+    }
+
+    private int EvaluateBinaryExpression(BinaryExpressionSyntax root)
+    {
+        var left = EvaluateExpression(root.Left);
+        var right = EvaluateExpression(root.Right);
+
+        // ReSharper disable once SwitchExpressionHandlesSomeKnownEnumValuesWithExceptionInDefault
+        return root.OperatorToken.Kind switch
+        {
+            SyntaxKind.PlusToken => left + right,
+            SyntaxKind.MinusToken => left - right,
+            SyntaxKind.StarToken => left * right,
+            SyntaxKind.SlashToken => left / right,
+            _ => throw new InvalidOperationException($"Unexpected binary operator {root.OperatorToken.Kind}")
+        };
+    }
+
+    private static int EvaluateLiteralExpression(LiteralExpressionSyntax root)
+    {
+        return root.LiteralToken.Value as int? ??
+               throw new InvalidOperationException();
+    }
+}
--- mc/Program.cs
+++ mc/Program.cs
@@ -27,7 +27,7 @@ internal static class Program
             }
             else
             {
-                syntaxTree.Root.PrettyPrint();
+                Console.WriteLine(new Evaluator(syntaxTree.Root).Evaluate());
             }
         }
     }
