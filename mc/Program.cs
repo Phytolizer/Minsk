2b881e0c5e0679c2f0c426184c3b0362de959eb9
Kyle Coffey <me@phytolizer.dev>
2022-03-20 11:53:25 -0500

    simple REPL improvements

--- mc/Program.cs
+++ mc/Program.cs
@@ -6,6 +6,8 @@ internal static class Program
 {
     private static void Main()
     {
+        var showTree = false;
+
         while (true)
         {
             Console.Write("> ");
@@ -13,6 +15,17 @@ internal static class Program
             if (line == null)
             {
                 break;
+            }
+
+            switch (line)
+            {
+                case "#showTree":
+                    showTree = !showTree;
+                    Console.WriteLine(showTree ? "Showing parse trees." : "Not showing parse trees.");
+                    continue;
+                case "#cls":
+                    Console.Clear();
+                    continue;
             }
 
             var parser = new Parser(line);
@@ -27,6 +40,11 @@ internal static class Program
             }
             else
             {
+                if (showTree)
+                {
+                    syntaxTree.Root.PrettyPrint();
+                }
+
                 Console.WriteLine(new Evaluator(syntaxTree.Root).Evaluate());
             }
         }
