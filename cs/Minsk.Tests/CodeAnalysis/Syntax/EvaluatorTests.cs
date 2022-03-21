d299f969aaf04e02215cf0dcfa094c48a96e3b94
Kyle Coffey <me@phytolizer.dev>
2022-03-21 14:22:04 -0500

    test evaluator briefly

---
+++ cs/Minsk.Tests/CodeAnalysis/Syntax/EvaluatorTests.cs
@@ -0,0 +1,41 @@
+using Minsk.CodeAnalysis;
+using Minsk.CodeAnalysis.Syntax;
+using Xunit;
+
+namespace Minsk.Tests.CodeAnalysis.Syntax;
+
+public class EvaluatorTests
+{
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("+1", 1)]
+    [InlineData("-1", -1)]
+    [InlineData("14 + 12", 26)]
+    [InlineData("12 - 3", 9)]
+    [InlineData("4 * 2", 8)]
+    [InlineData("9 / 3", 3)]
+    [InlineData("(10)", 10)]
+    [InlineData("12 == 3", false)]
+    [InlineData("3 == 3", true)]
+    [InlineData("12 != 3", true)]
+    [InlineData("3 != 3", false)]
+    [InlineData("false == false", true)]
+    [InlineData("true == false", false)]
+    [InlineData("false != false", false)]
+    [InlineData("true != false", true)]
+    [InlineData("true", true)]
+    [InlineData("false", false)]
+    [InlineData("!true", false)]
+    [InlineData("!false", true)]
+    [InlineData("(a = 10) * a", 100)]
+    public void EvaluatesCorrectValue(string text, object expected)
+    {
+        var syntaxTree = SyntaxTree.Parse(text);
+        var compilation = new Compilation(syntaxTree);
+        var variables = new Dictionary<VariableSymbol, object>();
+        var result = compilation.Evaluate(variables);
+
+        Assert.Empty(result.Diagnostics);
+        Assert.Equal(expected, result.Value);
+    }
+}
