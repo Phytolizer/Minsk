e39ddd5a17d7aec535734bd1321bfa8677328f3d
Kyle Coffey <me@phytolizer.dev>
2022-03-23 13:28:01 -0500

    [py] ensure line count is correct

---
+++ py/tests/analysis/text/test_source.py
@@ -0,0 +1,16 @@
+import pytest
+
+from minsk.analysis.text.source import SourceText
+
+
+@pytest.mark.parametrize(
+    "text,expected_line_count",
+    (
+        (".", 1),
+        (".\r\n", 2),
+        (".\r\n\r\n", 3),
+    ),
+)
+def test_includes_last_line(text: str, expected_line_count: int):
+    source_text = SourceText(text)
+    assert len(source_text.lines) == expected_line_count
