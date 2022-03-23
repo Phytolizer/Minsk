2773b4bd82ca6941db6ee06f5912991a02a010be
Kyle Coffey <me@phytolizer.dev>
2022-03-23 03:02:23 -0500

    [py] parser implementation

--- py/minsk/analysis/syntax/parser.py
+++ py/minsk/analysis/syntax/parser.py
@@ -1,4 +1,6 @@
 from minsk.analysis.syntax.expression import ExpressionSyntax
+from minsk.analysis.syntax.expressions.binary import BinaryExpressionSyntax
+from minsk.analysis.syntax.expressions.literal import LiteralExpressionSyntax
 from minsk.analysis.syntax.kind import SyntaxKind
 from minsk.analysis.syntax.lexer import Lexer
 from minsk.analysis.syntax.token import SyntaxToken
@@ -31,5 +33,42 @@ class Parser:
     def _current(self) -> SyntaxToken:
         return self._peek(0)
 
+    def _next_token(self) -> SyntaxToken:
+        current = self._current
+        self._position += 1
+        return current
+
+    def _match_token(self, kind: SyntaxKind) -> SyntaxToken:
+        if self._current.kind == kind:
+            return self._next_token()
+        return SyntaxToken(kind, self._current.position, "", None)
+
     def parse(self) -> ExpressionSyntax:
-        pass
+        return self._parse_expression()
+
+    def _parse_expression(self) -> ExpressionSyntax:
+        return self._parse_term()
+
+    def _parse_term(self) -> ExpressionSyntax:
+        left = self._parse_factor()
+
+        while self._current.kind in (SyntaxKind.PlusToken, SyntaxKind.MinusToken):
+            operator_token = self._next_token()
+            right = self._parse_factor()
+            left = BinaryExpressionSyntax(left, operator_token, right)
+
+        return left
+
+    def _parse_factor(self) -> ExpressionSyntax:
+        left = self._parse_primary_expression()
+
+        while self._current.kind in (SyntaxKind.StarToken, SyntaxKind.SlashToken):
+            operator_token = self._next_token()
+            right = self._parse_primary_expression()
+            left = BinaryExpressionSyntax(left, operator_token, right)
+
+        return left
+
+    def _parse_primary_expression(self) -> ExpressionSyntax:
+        number_token = self._match_token(SyntaxKind.NumberToken)
+        return LiteralExpressionSyntax(number_token)
