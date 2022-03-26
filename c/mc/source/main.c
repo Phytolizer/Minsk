3ad2ca8e2b261247c759101489459013be0be162
Kyle Coffey <me@phytolizer.dev>
2022-03-26 08:31:52 -0500

    [c] meta commands

--- c/mc/source/main.c
+++ c/mc/source/main.c
@@ -8,19 +8,33 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void) {
   char *line = NULL;
   size_t line_len = 0;
+  bool show_tree = false;
   while (true) {
     printf("> ");
     if (!util_read_line(&line, &line_len, stdin)) {
       break;
     }
 
+    if (strcmp(line, "#showTree") == 0) {
+      show_tree = !show_tree;
+      printf("%s\n",
+             show_tree ? "Showing parse trees." : "Not showing parse trees.");
+      continue;
+    } else if (strcmp(line, "#cls") == 0) {
+      printf("\x1b[2J\x1b[0;0H");
+      continue;
+    }
+
     syntax_tree_t syntax_tree = syntax_tree_parse(line);
     diagnostic_bag_t diagnostics = syntax_tree.diagnostics;
-    syntax_node_pretty_print((syntax_node_t *)syntax_tree.root, stdout);
+    if (show_tree) {
+      syntax_node_pretty_print((syntax_node_t *)syntax_tree.root, stdout);
+    }
     if (diagnostics.length > 0) {
       styler_apply_fg(styler_fg_red, stdout);
       for (size_t i = 0; i < diagnostics.length; i++) {
