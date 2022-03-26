6348ca0ad15b41bfe7b771b0d47ec4eb91ad4141
Kyle Coffey <me@phytolizer.dev>
2022-03-26 14:41:33 -0500

    [c] lexes token pairs

--- c/minsk_test/source/analysis/syntax/lexer.c
+++ c/minsk_test/source/analysis/syntax/lexer.c
@@ -8,6 +8,7 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
   syntax_kind_t kind;
@@ -77,12 +78,70 @@ START_TEST(lexes_token_test) {
 }
 END_TEST
 
+static bool requires_separator(simple_token_t *t1, simple_token_t *t2) {
+  bool t1_is_keyword =
+      strstr(syntax_kind_to_string(t1->kind), "keyword") != NULL;
+  bool t2_is_keyword =
+      strstr(syntax_kind_to_string(t2->kind), "keyword") != NULL;
+
+  if ((t1_is_keyword || t1->kind == syntax_kind_identifier_token) &&
+      (t2_is_keyword || t2->kind == syntax_kind_identifier_token)) {
+    return true;
+  }
+
+  if (t1->kind == syntax_kind_number_token &&
+      t2->kind == syntax_kind_number_token) {
+    return true;
+  }
+
+  if ((t1->kind == syntax_kind_bang_token ||
+       t1->kind == syntax_kind_equals_token) &&
+      (t2->kind == syntax_kind_equals_equals_token ||
+       t2->kind == syntax_kind_equals_token)) {
+    return true;
+  }
+
+  return false;
+}
+
+START_TEST(lexes_token_pairs_test) {
+  simple_token_vector_t tokens = get_tokens();
+
+  for (size_t i = 0; i < tokens.length; i++) {
+    for (size_t j = 0; j < tokens.length; j++) {
+      if (requires_separator(&tokens.data[i], &tokens.data[j])) {
+        continue;
+      }
+
+      sds text = sdscatfmt(sdsempty(), "%s%s", tokens.data[i].text,
+                           tokens.data[j].text);
+      syntax_token_vector_t lexed_tokens = syntax_tree_parse_tokens(text);
+      sdsfree(text);
+      ck_assert_int_eq(lexed_tokens.length, 2);
+      ck_assert_int_eq(lexed_tokens.data[0].base.kind, tokens.data[i].kind);
+      ck_assert_str_eq(lexed_tokens.data[0].text, tokens.data[i].text);
+      ck_assert_int_eq(lexed_tokens.data[1].base.kind, tokens.data[j].kind);
+      ck_assert_str_eq(lexed_tokens.data[1].text, tokens.data[j].text);
+      token_free(&lexed_tokens.data[0]);
+      token_free(&lexed_tokens.data[1]);
+      syntax_token_vector_free(&lexed_tokens);
+    }
+  }
+
+  free(tokens.data);
+}
+END_TEST
+
 Suite *lexer_suite(void) {
   Suite *s = suite_create("Lexer");
 
   TCase *tc_lexes_token = tcase_create("lexes token");
   tcase_add_test(tc_lexes_token, lexes_token_test);
   suite_add_tcase(s, tc_lexes_token);
+
+  TCase *tc_lexes_token_pairs = tcase_create("lexes token pairs");
+  tcase_add_test(tc_lexes_token_pairs, lexes_token_pairs_test);
+  suite_add_tcase(s, tc_lexes_token_pairs);
 
   return s;
 }
