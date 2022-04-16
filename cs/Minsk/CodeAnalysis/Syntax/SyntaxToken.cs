9c91df7b6182d5523ed74536e267c5f238c8cb27
Kyle Coffey <me@phytolizer.dev>
2022-04-16 04:34:30 -0500

    update editorconfig

--- cs/Minsk/CodeAnalysis/Syntax/SyntaxToken.cs
+++ cs/Minsk/CodeAnalysis/Syntax/SyntaxToken.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+using System.Text;
 using Minsk.CodeAnalysis.Text;
 
 namespace Minsk.CodeAnalysis.Syntax;
--- cs/mc/Program.cs
+++ cs/mc/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+using System.Text;
 using Minsk.CodeAnalysis;
 using Minsk.CodeAnalysis.Syntax;
 using Minsk.CodeAnalysis.Text;
