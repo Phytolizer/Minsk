b3e278cda4adbfe80a9ae90a4b8adaca5bc25e54
Kyle Coffey <me@phytolizer.dev>
2023-04-13 14:53:43 -0500

    add test harness (doctest)

--- c2/minsk/include/minsk/code_analysis/syntax/token.h
+++ c2/minsk/include/minsk/code_analysis/syntax/token.h
@@ -18,5 +18,6 @@ typedef struct
 static inline minsk_text_span_t
 minsk_syntax_token_span(minsk_syntax_token_t tok)
 {
-  return (minsk_text_span_t){tok.position, tok.text.length};
+  minsk_text_span_t span = {(size_t)tok.position, tok.text.length};
+  return span;
 }
---
+++ c2/tests/src/code_analysis/syntax/lexer.cpp
@@ -0,0 +1,7 @@
+extern "C"
+{
+#include <minsk/code_analysis/syntax/lexer.h>
+}
+#include <doctest.h>
+
+#include "minsk-tests/doctest-ext.hpp"
