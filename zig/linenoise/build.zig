1edff1f0e30b6965211c13703a17467a8eba6bc5
Kyle Coffey <me@phytolizer.dev>
2023-04-13 22:53:26 -0500

    TODO: blocked on ziglyph

--- zig/linenoise/build.zig
+++ zig/linenoise/build.zig
@@ -1,6 +1,10 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build, target: std.zig.CrossTarget, optimize: std.builtin.Mode) *std.Build.CompileStep {
+pub fn build(b: *std.Build, target: std.zig.CrossTarget, optimize: std.builtin.Mode) struct {
+    raw: *std.Build.CompileStep,
+    lib: *std.Build.InstallArtifactStep,
+    inc: *std.Build.InstallFileStep,
+} {
     const this_dir = comptime std.fs.path.dirname(@src().file) orelse ".";
     const result = b.addStaticLibrary(.{
         .name = "linenoise",
@@ -10,7 +14,7 @@ pub fn build(b: *std.Build, target: std.zig.CrossTarget, optimize: std.builtin.M
     result.linkLibC();
     result.addCSourceFile(this_dir ++ "/linenoise-ship.c", &.{"-DUSE_UTF8"});
     result.addIncludePath(this_dir);
-    b.installArtifact(result);
-    b.addInstallHeaderFile(this_dir ++ "/linenoise.h", "linenoise.h");
-    return result;
+    const lib = b.addInstallArtifact(result);
+    const inc = b.addInstallHeaderFile(this_dir ++ "/linenoise.h", "linenoise.h");
+    return .{ .raw = result, .lib = lib, .inc = inc };
 }
