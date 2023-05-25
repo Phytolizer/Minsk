cdb8ff8398ce8e13434bba7d090125e2176cd1f3
Kyle Coffey <me@phytolizer.dev>
2023-05-25 09:12:23 -0500

    tweak type printer to remove prefix

--- ocaml/lib/runtime/value.ml
+++ ocaml/lib/runtime/value.ml
@@ -1,6 +1,7 @@
 type t = Int of int | Bool of bool
-type ty = TyInt | TyBool [@@deriving show { with_path = false }]
+type ty = TyInt | TyBool
 
+let show_ty = function TyInt -> "Int" | TyBool -> "Bool"
 let tyof = function Int _ -> TyInt | Bool _ -> TyBool
 let get_int = function Int i -> i | _ -> failwith "not int"
 let get_bool = function Bool b -> b | _ -> failwith "not bool"
