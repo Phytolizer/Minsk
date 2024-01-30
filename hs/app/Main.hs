3c21f635c7c0a6ea2b7b48d5e6d8f67410362a02
Kyle Coffey <me@phytolizer.dev>
2024-01-30 15:19:30 -0600

    refactor: monad-loops instead of manual loop

--- hs/app/Main.hs
+++ hs/app/Main.hs
@@ -2,28 +2,32 @@ module Main where
 
 import BasicPrelude
 import Control.Exception (try)
+import Control.Monad.Loops (unfoldM_)
 import GHC.IO.Handle (hFlush)
 import GHC.IO.Handle.FD (stdout)
 import Minsk.SyntaxNode (SyntaxNode (SyntaxNode), pprint)
 import qualified Minsk.SyntaxTree as SyntaxTree
 
+done :: IO (Maybe ())
+done = return Nothing
+
+next :: IO (Maybe ())
+next = return $ Just ()
+
 main :: IO ()
-main = loop ()
-  where
-    loop :: () -> IO ()
-    loop () = do
-      putStr "> "
-      hFlush stdout
-      input <- try getLine
-      case input of
-        Left e ->
-          if isEOFError e
-            then do
-              putStrLn ""
-              return ()
-            else ioError e
-        Right line -> do
-          let ast = SyntaxTree.parse line
-          mapM_ print ast.diagnostics
-          pprint stdout $ SyntaxNode ast.root
-          loop ()
+main = unfoldM_ do
+  putStr "> "
+  hFlush stdout
+  input <- try getLine
+  case input of
+    Left e ->
+      if isEOFError e
+        then do
+          putStrLn ""
+          done
+        else ioError e
+    Right line -> do
+      let ast = SyntaxTree.parse line
+      mapM_ print ast.diagnostics
+      pprint stdout $ SyntaxNode ast.root
+      next
